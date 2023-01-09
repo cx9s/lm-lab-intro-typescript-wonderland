a820f5b0e5924db94c361df98cb1398b3095be6b
cx9s <cx9918@gmail.com>
2023-01-09 23:15:31 +0000

    done index, 1 and 2

--- index.ts
+++ index.ts
@@ -1,46 +1,46 @@
-import { clear, print, askQuestion } from './console';
-import { enterTheRabbitHole } from './src/1_rabbit_hole';
+import { clear, print, askQuestion } from "./console";
+import { enterTheRabbitHole } from "./src/1_rabbit_hole";
 
 export function haveAdventures(): void {
-	clear(false);
-	print('--------------------------');
-	print('| Welcome to Wonderland! |');
-	print('--------------------------');
+  clear(false);
+  print("--------------------------");
+  print("| Welcome to Wonderland! |");
+  print("--------------------------");
 
-	askQuestion(`What's your name? `, failImmediately); // 👉 FIXME ❌
+  askQuestion(`What's your name? `, startAdventure); // 👉 FIXME ❌
 }
 
 function failImmediately(): void {
-	clear(false);
-	return endAdventure();
+  clear(false);
+  return endAdventure();
 }
 
-const numberBiggerThanThree: any = '11'; // 👉 FIXME ❌
+const numberBiggerThanThree: Number = 11; // 👉 FIXME ❌
 
 // 👉 FIXME ❌
-function checkEntryCodeIsBiggerThanThree(code: any) {
-	return code > '3'; // 👉 FIXME ❌
+function checkEntryCodeIsBiggerThanThree(code: Number) {
+  return code > 3; // 👉 FIXME ❌
 }
 
 function startAdventure(name: string): void {
-	if (name && name.length > 0) {
-		if (checkEntryCodeIsBiggerThanThree(numberBiggerThanThree)) {
-			return enterTheRabbitHole(name);
-		} else {
-			print('***************************************');
-			print(`Hi ${name}. Sadly, the entry code failed! ☹`);
-			return endAdventure();
-		}
-	} else {
-		print(`I don't know who you are!`);
-		return endAdventure();
-	}
+  if (name && name.length > 0) {
+    if (checkEntryCodeIsBiggerThanThree(numberBiggerThanThree)) {
+      return enterTheRabbitHole(name);
+    } else {
+      print("***************************************");
+      print(`Hi ${name}. Sadly, the entry code failed! ☹`);
+      return endAdventure();
+    }
+  } else {
+    print(`I don't know who you are!`);
+    return endAdventure();
+  }
 }
 
 export function endAdventure(): void {
-	print('***************************************');
-	print('You did not make it through Wonderland. 😭');
-	askQuestion('Press ENTER to restart! ', haveAdventures);
+  print("***************************************");
+  print("You did not make it through Wonderland. 😭");
+  askQuestion("Press ENTER to restart! ", haveAdventures);
 }
 
 haveAdventures();
